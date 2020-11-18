f68b9ad7d1bbeaebf69532cbfcdc7a37ebb74479
urazzak <59906778+urazzak@users.noreply.github.com>
2020-11-17 20:30:53 -0600

    Add files via upload

--- Assignment 09/Activity#1.py
+++ Assignment 09/Activity#1.py
@@ -1,1 +1,18 @@
+def display_order(names):
+    print (names)
 
+
+def display_reverse(names):
+    names.reverse()
+    print (names)
+
+
+def main():
+    names = ["Razzak", "Umer", "Humza", "Yousuf", "Amin",
+             "Naheed", "Paryani", "Sana", "Adnan", "Marium"]
+    display_order(names)
+    display_reverse(names)
+
+# Main
+# This program displays list of names in order and reverse.
+main()
