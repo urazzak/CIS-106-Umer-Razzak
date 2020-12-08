695042de9ad0055cfe19ffc89518316a887b0f84
urazzak <59906778+urazzak@users.noreply.github.com>
2020-12-07 23:28:00 -0600

    Add files via upload

--- Assignment 10/Activity#1.py
+++ Assignment 10/Activity#1.py
@@ -1,1 +1,18 @@
+def get_input(array):
+    for index in range(len(array)):
+        array[index] = input("Enter a value: ")
 
+
+def display_results(array):
+    for index in range(len(array)):
+        print(array[index])
+
+
+def main():
+    array = [None] * 3
+    get_input(array)
+    display_results(array)
+
+# Main
+# This Program creates a dynamic array
+main()
