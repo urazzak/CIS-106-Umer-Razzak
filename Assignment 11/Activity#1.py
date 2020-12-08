e238919f39ddfc79af01977a25f160a2ac83c54e
urazzak <59906778+urazzak@users.noreply.github.com>
2020-12-08 17:27:20 -0600

    Add files via upload

--- Assignment 11/Activity#1.py
+++ Assignment 11/Activity#1.py
@@ -1,1 +1,25 @@
+def get_name():
+    name = str(input("Enter your first and last name: "))
 
+    return name
+
+
+def split_name(name):
+    split = name.split()
+
+    return split
+
+
+def display_results(name, split):
+    print ("{},".format(split[1]), "{}.".format(name[0]))
+
+
+def main():
+    name = get_name()
+    split = split_name(name)
+    display_results(name, split)
+
+
+# Main
+# This program prompts user for name and display it back
+main()
