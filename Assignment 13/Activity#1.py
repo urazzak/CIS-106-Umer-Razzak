c6101a4fd11a7b2b532dfe7d8bbd96fea4347515
urazzak <59906778+urazzak@users.noreply.github.com>
2020-12-11 22:15:36 -0600

    Add files via upload

--- Assignment 13/Activity#1.py
+++ Assignment 13/Activity#1.py
@@ -1,1 +1,16 @@
+class Employee:
+    empcount = 0 
 
+    def __init__(self, firstname, lastname, salary):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.salary = salary
+        Employee.empcount += 1
+    
+    def displayEmployee(self):
+      print ("First Name: ", self.firstname, " Last Name: ", self.lastname,  ", Salary: $", self.salary)
+
+
+emp1 = Employee(input("Enter the First Name: "),
+                input("Enter the Last Name: "), (input("Enter salary: ")))
+emp1.displayEmployee()
